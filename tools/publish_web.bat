bd08d848fbe36cb5bb0c23517d4e2ad4ffd5df47
supark0403 <supark0403@naver.com>
2026-09-23 19:42:26 +0900

    v1.1 표기, PWA 서비스워커 캐시 제거

--- tools/publish_web.bat
+++ tools/publish_web.bat
@@ -7,6 +7,7 @@ if not exist "%OUT%" mkdir "%OUT%"
 echo [1/3] Godot web export...
 "E:\Godot\Godot_v4.7.2-stable_win64.exe\Godot_v4.7.2-stable_win64_console.exe" --headless --path "%PROJ%" --export-release Web "%OUT%\index.html"
 if errorlevel 1 exit /b 1
+del /q "%OUT%\*.import" 2>nul
 set CLONE=%TEMP%\tetris-pages
 echo [2/3] gh-pages 브랜치 구성...
 if exist "%CLONE%" rmdir /s /q "%CLONE%"
