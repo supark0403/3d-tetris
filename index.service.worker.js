489fcfb225c2fe514570ba5a535165bd7e5ead55
supark0403 <supark0403@users.noreply.github.com>
2026-09-23 19:38:37 +0900

    web build: T스폰·재시작 수정 반영

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1790159691|1998843';
+const CACHE_VERSION = '1790159915|2142756';
 /** @type {string} */
 const CACHE_PREFIX = '3D Tetris-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
